54f634e4f9b01e321f3f075f0ba3313858b55032
ZenithGD <darimarcos456@hotmail.com>
2022-05-18 19:54:42 +0200

    resize and independent player move

--- src/app/board/board.page.ts
+++ src/app/board/board.page.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import * as PIXI from 'pixi.js';
-import { fromEvent, tap } from 'rxjs';
+import { fromEvent, tap, Observable, Subscription } from 'rxjs';
 import { Actor } from './actor';
 import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 import { Player } from './player';
 import { ActivatedRoute } from '@angular/router';
+
+class Game {
+
+}
 
 @Component({
   selector: 'app-board',
@@ -12,75 +16,107 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./board.page.scss'],
 })
 export class BoardPage implements OnInit {
-
   numPlayers: any;
 
-  private app: PIXI.Application = new PIXI.Application({
-    width: window.innerHeight,
-    height: window.innerWidth - 30,
-    backgroundColor: 0xffffff
-  });
+  private app: PIXI.Application;
 
-  private actors: Actor[] = [];
+  private actors: Player[] = [];
 
-  constructor(private screenOrientation: ScreenOrientation, private activatedRoute: ActivatedRoute) {
+  constructor(
+    private screenOrientation: ScreenOrientation,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
-   }
+    this.app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      backgroundColor: 0xffffff,
+      antialias: true,
+      resizeTo: window
+    });
+  }
 
   ngOnInit() {
     this.numPlayers = this.activatedRoute.snapshot.paramMap.get('numJugadores');
-    const background = PIXI.Sprite.from('../../assets/tableroFinalCentroCompleto.png');
-    background.width = this.app.screen.width;
-    background.height = this.app.screen.height;
-    console.log('BACK W: ', background.width, 'APP: ', this.app.screen.width);
-    console.log('BACK H: ', background.height, 'APP: ', this.app.screen.height);
-    this.app.stage.addChild(background);
+    
+    let bg = PIXI.Sprite.from(
+      '../../assets/tableroFinalCentroCompleto.png'
+    );
+    bg.width = window.innerWidth;
+    bg.height = window.innerHeight;
+    //console.log('BACK W: ', bg.width, 'APP: ', this.app.screen.width);
+    //console.log('BACK H: ', bg.height, 'APP: ', this.app.screen.height);
+    this.app.stage.addChild(bg);
+    
     const stitch = PIXI.Texture.from('../../assets/stitch.png');
     const cube = PIXI.Texture.from('../../assets/cube.png');
-    document.body.appendChild(this.app.view);
-    for(let i = 0; i < this.numPlayers; i++){
-      if(i%2 === 0){
-        this.actors.push(new Player(stitch,this.app));
-      }
-      else {
-        //this.actors.push(new Player(cube, this.app));
+    for (let i = 0; i < this.numPlayers; i++) {
+      if (i % 2 === 0) {
+        this.actors.push(new Player(stitch, this.app));
+      } else {
+        this.actors.push(new Player(cube, this.app));
       }
     }
-    this.initActors();
+
+    //this.initActors();
+
+    // main game loop
+    this.app.ticker.add(function() {
+      // resize background
+      bg.width = window.innerWidth;
+      bg.height = window.innerHeight;
+    });
+
+    document.body.appendChild(this.app.view);
+
+    this.actors[0].movePlayer(0, 0);
+    this.actors[1].movePlayer(100, 0);
+    this.actors[2].movePlayer(0, 100);
+    this.actors[3].movePlayer(200, 0);
+    this.actors[4].movePlayer(100, 400);
+    this.actors[5].movePlayer(300, 200);
   }
 
-
   initActors() {
-
-
     // Call actor update method
-    for (let i = 0; i < this.actors.length; i++){
+    for (let i = 0; i < this.actors.length; i++) {
       const actor = this.actors[i];
       this.app.stage.addChild(actor);
-      this.app.ticker.add( (e) => actor.update(e));
+      this.app.ticker.add((e) => actor.update(e));
     }
 
     fromEvent(document, 'keydown')
-    .pipe(tap((e: any) => {
-      for (let i = 0; i < this.actors.length; i++) {
-        const actor = this.actors[i];
-        actor.onKeydown(e);
-      }
-    })).subscribe();
+      .pipe(
+        tap((e: any) => {
+          for (let i = 0; i < this.actors.length; i++) {
+            const actor = this.actors[i];
+            actor.onKeydown(e);
+          }
+        })
+      )
+      .subscribe();
 
     fromEvent(document, 'keyup')
-        .pipe(tap((e: any) => {
+      .pipe(
+        tap((e: any) => {
           for (let i = 0; i < this.actors.length; i++) {
             const actor = this.actors[i];
             actor.onKeyup(e);
           }
-        })).subscribe();
-
-
+        })
+      )
+      .subscribe();
   }
 
-  unlockOrientation(){
+  unlockOrientation() {
     this.screenOrientation.unlock();
   }
 
+  destroy() {
+    this.app.destroy();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy();
+  }
 }
